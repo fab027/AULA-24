f857d47686d34b06bd3a63abcbeeeee4cf43b26b
Fabricio <129768096+fab027@users.noreply.github.com>
2024-11-11 20:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function AlertaNome() {
+    var Nome = document.getElementById('inputNome').value;
+    var Sobrenome = document.getElementById('inputSobrenome').value;
+window.alert('O seu Nome é ' + Nome + ' ' + Sobrenome)
+
+}
+
+
